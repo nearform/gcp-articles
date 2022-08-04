9d3ec81452cb0de7a57a8b1104d73dd73e2527a9
Gustavo Caldeira <gustavo.caldeira@nearform.com>
2022-08-04 15:10:52 +0100

    Function go

--- 3-tiers-serverless/terraform/build-app.tf
+++ 3-tiers-serverless/terraform/build-app.tf
@@ -1,6 +1,6 @@
 data "archive_file" "app_zip" {
   type        = "zip"
-  source_dir  = "../../app"
+  source_dir  = "../../function"
   output_path = "app.zip"
 }
 
@@ -25,13 +25,28 @@ resource "google_service_account" "app_sa" {
 }
 
 resource "google_cloudfunctions_function" "app" {
-  name                  = var.app_name
+  name                  = format("%s-%s", var.app_name, "get-counters")
   runtime               = "go116"
 
   source_archive_bucket = google_storage_bucket.functions_src.name
   source_archive_object = google_storage_bucket_object.app.name
   trigger_http          = true
-  entry_point           = "main"
+  entry_point           = "GetCounters"
+  vpc_connector         = google_vpc_access_connector.connector.name
+  environment_variables = {
+    REDIS_ADDR = "${google_redis_instance.data.host}:${google_redis_instance.data.port}"
+  }
+  service_account_email = google_service_account.app_sa.email
+}
+
+resource "google_cloudfunctions_function" "app" {
+  name                  = format("%s-%s", var.app_name, "set-counter")
+  runtime               = "go116"
+
+  source_archive_bucket = google_storage_bucket.functions_src.name
+  source_archive_object = google_storage_bucket_object.app.name
+  trigger_http          = true
+  entry_point           = "SetCounters"
   vpc_connector         = google_vpc_access_connector.connector.name
   environment_variables = {
     REDIS_ADDR = "${google_redis_instance.data.host}:${google_redis_instance.data.port}"
