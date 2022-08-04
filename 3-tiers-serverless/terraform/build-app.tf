403c615e7c92d7ac4fbac261e618bc6d73dd29d3
Pehesi97 <pedro.silva@wisenet.inf.br>
2022-08-04 11:35:09 -0300

    3 tiers functions complete

--- 3-tiers-serverless/terraform/build-app.tf
+++ 3-tiers-serverless/terraform/build-app.tf
@@ -24,7 +24,7 @@ resource "google_service_account" "app_sa" {
   account_id = var.app_name
 }
 
-resource "google_cloudfunctions_function" "app" {
+resource "google_cloudfunctions_function" "get_counters" {
   name                  = format("%s-%s", var.app_name, "get-counters")
   runtime               = "go116"
 
@@ -39,14 +39,14 @@ resource "google_cloudfunctions_function" "app" {
   service_account_email = google_service_account.app_sa.email
 }
 
-resource "google_cloudfunctions_function" "app" {
+resource "google_cloudfunctions_function" "set_counter" {
   name                  = format("%s-%s", var.app_name, "set-counter")
   runtime               = "go116"
 
   source_archive_bucket = google_storage_bucket.functions_src.name
   source_archive_object = google_storage_bucket_object.app.name
   trigger_http          = true
-  entry_point           = "SetCounters"
+  entry_point           = "SetCounter"
   vpc_connector         = google_vpc_access_connector.connector.name
   environment_variables = {
     REDIS_ADDR = "${google_redis_instance.data.host}:${google_redis_instance.data.port}"
@@ -54,8 +54,14 @@ resource "google_cloudfunctions_function" "app" {
   service_account_email = google_service_account.app_sa.email
 }
 
-resource "google_cloudfunctions_function_iam_member" "app_allow_api_gateway" {
-  cloud_function = google_cloudfunctions_function.app.name
-  member         = "serviceAccount:${google_service_account.app_sa.email}"
+resource "google_cloudfunctions_function_iam_member" "set_counter_allow_public" {
+  cloud_function = google_cloudfunctions_function.set_counter.name
+  member         = "allUsers"
+  role           = "roles/cloudfunctions.invoker"
+}
+
+resource "google_cloudfunctions_function_iam_member" "get_counters_allow_public" {
+  cloud_function = google_cloudfunctions_function.get_counters.name
+  member         = "allUsers"
   role           = "roles/cloudfunctions.invoker"
 }
--- 3-tiers-serverless/terraform/outputs.tf
+++ 3-tiers-serverless/terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "app_trigger_url" {
-  value = google_cloudfunctions_function.app.https_trigger_url
+output "set_trigger_url" {
+  value = google_cloudfunctions_function.set_counter.https_trigger_url
+}
+
+output "get_trigger_url" {
+  value = google_cloudfunctions_function.get_counters.https_trigger_url
 }
